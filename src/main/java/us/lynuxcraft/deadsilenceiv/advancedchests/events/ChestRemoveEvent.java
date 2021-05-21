b7485c3386864c880859efbcf84472121ad8a02f
DeadSilenceIV <barreto-h2@hotmail.com>
2021-05-21 12:52:20 -0500

    New ChestRemoveEvent was added.

---
+++ src/main/java/us/lynuxcraft/deadsilenceiv/advancedchests/events/ChestRemoveEvent.java
@@ -0,0 +1,22 @@
+package us.lynuxcraft.deadsilenceiv.advancedchests.events;
+
+import us.lynuxcraft.deadsilenceiv.advancedchests.chest.AdvancedChest;
+
+/**
+ * Called when an AdvancedChest is being removed
+ */
+public class ChestRemoveEvent{
+    /**
+     * Gets the {@link AdvancedChest} instance.
+     */
+    public AdvancedChest getChest(){
+        return null;
+    }
+
+    /**
+     * Cancels the event.
+     *
+     * @param b if should be cancelled or not.
+     */
+    public void setCancelled(boolean b) {}
+}
